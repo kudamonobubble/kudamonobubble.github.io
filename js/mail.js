d0bf849973251fa2a062c51fa511929722e1a2b6
CWKanime <A112070081@mail.shu.edu.tw>
2024-06-16 11:36:01 +0800

    06161

--- js/mail.js
+++ js/mail.js
@@ -1,28 +1,23 @@
 document.getElementById('email-form').addEventListener('submit', function(event) {
     event.preventDefault(); // 阻止表单默认提交行为
 
-    var email = document.getElementById('email').value;
-    var data = { email: email };
+    var formData = new FormData(this);
 
-    fetch('YOUR_ZAPIER_WEBHOOK_URL', {
+    fetch(this.action, {
         method: 'POST',
-        headers: {
-            'Content-Type': 'application/json'
-        },
-        body: JSON.stringify(data)
+        body: formData
     })
     .then(response => {
         if (response.ok) {
             document.getElementById('success-message').style.display = 'block';
+            // 可以在这里执行其他成功后的操作，例如重置表单
+            // this.reset();
         } else {
-            response.text().then(text => {
-                console.error('Response not OK:', text);
-                alert('提交失败，请重试。');
-            });
+            throw new Error('提交失败，请重试。');
         }
     })
     .catch(error => {
         console.error('Fetch错误:', error);
-        alert('提交失败，请重试。');
+        alert(error.message);
     });
 });
